e705e84315a7ac855119a872cbeab52a5c24e07d
makachikava <chikavamakanaka@gmail.com>
2023-05-22 19:42:28 -0700

    created navbar in new Vue.js app

---
+++ vue-start-realrealty/src/components/navBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="navbar sticky-top bg-body-tertiary bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand text-light fs-2" href="#">{{ logo }}</a>
+            </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            logo: 'Real Realty',
+            url: 'home.html'
+        }
+        
+    }
+}
+
+</script>
