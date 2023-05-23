06732e58daf37283ff95f968b29575d8f862e566
makachikava <chikavamakanaka@gmail.com>
2023-05-23 16:17:19 -0700

    added home img to vue app and installed axios

--- vue-start-realrealty/src/App.vue
+++ vue-start-realrealty/src/App.vue
@@ -1,16 +1,18 @@
 <template>
+    <CityImg/>
     <navBar/> 
-    <p>simple text</p>
+    <p>Page content here</p>
 </template>
 
 <script>
 import navBar from './components/navBar.vue';
-
+import CityImg from './components/cityImg.vue';
 
 export default {
     components: {
-        navBar
-    },
+    navBar,
+    CityImg
+},
 
 }
 </script>
---
+++ vue-start-realrealty/src/components/cityImg.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80" alt="">
+</template>
