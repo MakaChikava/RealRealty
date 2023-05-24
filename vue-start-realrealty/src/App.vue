d7c6252e8b347ac2ce0aab81de2670dd33a45d02
makachikava <chikavamakanaka@gmail.com>
2023-05-23 17:19:32 -0700

    made api call and is showing in console

--- vue-start-realrealty/src/App.vue
+++ vue-start-realrealty/src/App.vue
@@ -2,16 +2,18 @@
     <CityImg/>
     <navBar/> 
     <p>Page content here</p>
+    <dataProperty/>
 </template>
 
 <script>
 import navBar from './components/navBar.vue';
 import CityImg from './components/cityImg.vue';
-
+import dataProperty from './components/dataProperty.vue';
 export default {
     components: {
     navBar,
-    CityImg
+    CityImg,
+    dataProperty
 },
 
 }
---
+++ vue-start-realrealty/src/components/dataProperty.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Property Data</h1>
+    </div>
+
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+    setup() {
+    axios
+        .get('http://localhost:8000')
+        .then((res)=>{
+            console.log(res.data )
+        })
+    }
+}
+</script>
