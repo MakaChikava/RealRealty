477d050f1b9ee992f92673eeac00e54a4ae4cfa0
makachikava <chikavamakanaka@gmail.com>
2023-06-09 12:54:04 -0700

    still working on create

--- vue-start-realrealty/src/components/dataProperty.vue
+++ vue-start-realrealty/src/components/dataProperty.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <h1>Create Form</h1>
+<form action="">
+    <input type="text" v-model="property.address" placeholder="address">
+    <input type="text" v-model="property.city" placeholder="city">
+    <input type="text" v-model="property.img" placeholder="img">
+    <input type="text" v-model="property.state" placeholder="state">
+    <input type="text" v-model="property.type" placeholder="type">
+    <input type="text" v-model="property.zip" placeholder="zip">
+</form>
+<button @click="addProperty()"> submit </button>
+
         <h1>Property Data</h1>
         <form>
             <input type="text">
@@ -37,6 +48,16 @@ export default {
     {
         return {
             properties:[],
+            property: {
+                address: "",
+                city: "",
+                img: "",
+                price: "",
+                state: "",
+                type: "",
+                zip: ""
+
+            }
         }
     },
 
@@ -47,7 +68,23 @@ export default {
                 .then((res)=>{
                     this.properties=res.data
                 })
-        }
+        },
+        addProperty(){
+            axios
+                .post("http://localhost:8000/house", {
+                    address: this.property.address,
+                    city: this.property.city,
+                    img: this.property.img,
+                    price: this.property.price,
+                    state: this.property.state,
+                    type: this.property.type,
+                    zip: this.property.zip
+                })
+                .then((res)=>{
+                    if(res.status ==201) {
+                        console.log("succesful")
+                    }
+        })}
     },
 
     mounted:function() 
