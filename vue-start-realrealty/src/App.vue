22d65fa760135c84dd358585ac18a4554763dbec
makachikava <chikavamakanaka@gmail.com>
2023-06-14 16:14:22 -0700

    updated vue and deleted modal test component

--- vue-start-realrealty/src/App.vue
+++ vue-start-realrealty/src/App.vue
@@ -1,21 +1,22 @@
 <template>
+    
+    <div>
     <CityImg/>
     <navBar/> 
-    <!-- <modalTest/> -->
-    <dataProperty/>
+    <dataProperty/> 
+    </div>
+    
 </template>
 
 <script>
 import navBar from './components/navBar.vue';
 import CityImg from './components/cityImg.vue';
 import dataProperty from './components/dataProperty.vue';
-// import modalTest from './components/modalTest.vue';
 export default {
     components: {
     navBar,
     CityImg,
     dataProperty,
-    // modalTest
 },
 
 }
